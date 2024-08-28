917353f72d53b2d72ac5e2ee50d04c5c2767ef09
Krishnaraj004 <142392358+Krishnaraj004@users.noreply.github.com>
2024-08-28 12:19:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,91 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css'
+function App() {
+  return (
+    <div className="container">
+     <div className='row'>
+       <div className='col-1 black'><span style={{ fontSize:"50px",color:"white"}}>&#9814;</span></div>
+       <div className='col-1'> <span style={{ fontSize:"50px"}}>&#9822;</span></div>
+       <div className='col-1 black'> <span style={{ fontSize:"50px",color:"white"}}>&#9815;</span></div>
+       <div className='col-1'> <span style={{ fontSize:"50px"}}>&#9819;</span></div> 
+       <div className='col-1 black'><span style={{ fontSize:"50px",color:"white"}}>&#9818;</span></div> 
+       <div className='col-1'> <span style={{ fontSize:"50px"}}>&#9815;</span></div>
+       <div className='col-1 black'><span style={{ fontSize:"50px",color:"white"}}>&#9822;</span></div>
+       <div className='col-1'> <span style={{ fontSize:"50px"}}>&#9814;</span></div>
+     </div>	
+     <div className='row'>
+       <div className='col-1 '><span style={{ fontSize:"50px" ,color:"black"}}>&#9817;</span></div>
+       <div className='col-1 black'><span style={{ fontSize:"50px" ,color:"white"}}>&#9817;</span></div>
+       <div className='col-1 '><span style={{ fontSize:"50px" ,color:"black"}}>&#9817;</span></div>
+       <div className='col-1 black'><span style={{ fontSize:"50px" ,color:"white"}}>&#9817;</span></div>
+       <div className='col-1 '><span style={{ fontSize:"50px" ,color:"black"}}>&#9817;</span></div>
+       <div className='col-1 black'><span style={{ fontSize:"50px" ,color:"white"}}>&#9817;</span></div>
+       <div className='col-1 '><span style={{ fontSize:"50px" ,color:"black"}}>&#9817;</span></div>
+       <div className='col-1 black'><span style={{ fontSize:"50px" ,color:"white"}}>&#9817;</span></div>
+     </div>
+     <div className='row'>
+       <div className='col-1 black'></div>
+       <div className='col-1'></div>
+       <div className='col-1 black'></div>
+       <div className='col-1'></div> 
+       <div className='col-1 black'></div> 
+       <div className='col-1'></div>
+       <div className='col-1 black'></div>
+       <div className='col-1'></div>
+     </div>
+     <div className='row'>
+       <div className='col-1 '></div>
+       <div className='col-1 black'></div>
+       <div className='col-1 '></div>
+       <div className='col-1 black'></div> 
+       <div className='col-1 '></div> 
+       <div className='col-1 black'></div>
+       <div className='col-1 '></div>
+       <div className='col-1 black'></div>
+     </div>
+     <div className='row'>
+       <div className='col-1 black'></div>
+       <div className='col-1'></div>
+       <div className='col-1 black'></div>
+       <div className='col-1'></div> 
+       <div className='col-1 black'></div> 
+       <div className='col-1'></div>
+       <div className='col-1 black'></div>
+       <div className='col-1'></div>
+     </div>
+     <div className='row'>
+       <div className='col-1 '></div>
+       <div className='col-1 black'></div>
+       <div className='col-1 '></div>
+       <div className='col-1 black'></div> 
+       <div className='col-1 '></div> 
+       <div className='col-1 black'></div>
+       <div className='col-1 '></div>
+       <div className='col-1 black'></div>
+     </div>
+     <div className='row'>
+       <div className='col-1 black'><span style={{ fontSize:"50px" ,color:"white"}}>&#9817;</span></div>
+       <div className='col-1'><span style={{ fontSize:"50px" ,color:"black"}}>&#9817;</span></div>
+       <div className='col-1 black'><span style={{ fontSize:"50px" ,color:"white"}}>&#9817;</span></div>
+       <div className='col-1'><span style={{ fontSize:"50px" ,color:"black"}}>&#9817;</span></div>
+       <div className='col-1 black'><span style={{ fontSize:"50px" ,color:"white"}}>&#9817;</span></div>
+       <div className='col-1'><span style={{ fontSize:"50px" ,color:"black"}}>&#9817;</span></div>
+       <div className='col-1 black'><span style={{ fontSize:"50px" ,color:"white"}}>&#9817;</span></div>
+       <div className='col-1'><span style={{ fontSize:"50px" ,color:"black"}}>&#9817;</span></div>
+     </div>
+     <div className='row'>
+       <div className='col-1 '><span style={{ fontSize:"50px",color:"black"}}>&#9814;</span></div>
+       <div className='col-1 black'><span style={{ fontSize:"50px",color:"white"}}>&#9822;</span></div>
+       <div className='col-1 '><span style={{ fontSize:"50px",color:"black"}}>&#9815;</span></div>
+       <div className='col-1 black'><span style={{ fontSize:"50px",color:"white"}}>&#9818;</span></div> 
+       <div className='col-1 '><span style={{ fontSize:"50px",color:"black"}}>&#9819;</span></div> 
+       <div className='col-1 black'><span style={{ fontSize:"50px",color:"white"}}>&#9815;</span></div>
+       <div className='col-1 '><span style={{ fontSize:"50px"}}>&#9822;</span></div>
+       <div className='col-1 black'><span style={{ fontSize:"50px",color:"white"}}>&#9814;</span></div>
+     </div>
+         
+    </div>
+  );
+}
+
+export default App;
